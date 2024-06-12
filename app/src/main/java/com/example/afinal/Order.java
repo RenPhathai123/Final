665c040cbfa90c4183be1a358fe90d27eae3c6c2
RenPhathai123 <phatthairen@gmail.com>
2024-06-12 20:17:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/afinal/Order.java
@@ -0,0 +1,35 @@
+package com.example.afinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_order);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btnpdroduct = findViewById(R.id.buttonViewDetails4);
+        btnpdroduct.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Order.this, detail.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
